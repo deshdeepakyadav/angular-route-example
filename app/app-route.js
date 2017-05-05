3b86b3452cf6be0bd51e631d0bdf527ad81e8f66
Desh Deepak Yadav <deshdeepak2000@gmail.com>
2017-05-05 12:18:05 +0530

    ngMeta update
    ngMeta update

--- app/app-route.js
+++ app/app-route.js
@@ -1,24 +1,46 @@
-var myDataApp = angular.module('dataApp',['ngRoute','showDataCtrl','addCtrl','updateCtrl','removeCtrl']);
+var myDataApp = angular.module('dataApp',['ngRoute','ngMeta','showDataCtrl','addCtrl','updateCtrl','removeCtrl']);
 	
-	myDataApp.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+	myDataApp.config(['$routeProvider','ngMetaProvider',function($routeProvider,ngMetaProvider){
 		  $routeProvider.
 				when('/',{
 				templateUrl: 'partial/list.html',
-				controller: 'showDataController'				
+				controller: 'showDataController',
+				meta:   {
+						  //Sets 'Home Page' as the title when /home is open
+						  title: 'Home page', 
+						  description: 'This is the description of the home page!'
+					    }				
 				}).
 				when('/add',{
 				templateUrl: 'partial/add.html',
-				controller: 'addController'				
+				controller: 'addController',
+				meta:   {
+						  title: 'Add page', 
+						  description: 'This is the description of the Add page!'
+					    }				
 				}).
 				when('/modify',{
 				templateUrl: 'partial/modify.html',
-				controller: 'updateController'				
+				controller: 'updateController',
+				meta:   {
+						  title: 'Update page', 
+						  description: 'This is the description of the Update page!'
+					    }
 				}).
 				when('/delete',{
 				templateUrl: 'partial/delete.html',
-				controller: 'removeController'				
+				controller: 'removeController',
+				meta:   {
+						  title: 'Delete page', 
+						  description: 'This is the description of the Delete page!'
+					    }
 				}).
 				otherwise({redirectTo: '/'});
-				$locationProvider.html5Mode(true);
+				ngMetaProvider.useTitleSuffix(true);
+				ngMetaProvider.setDefaultTag('description', 'This is the default description');
+				ngMetaProvider.setDefaultTitle('ngMeta');
+				ngMetaProvider.setDefaultTitleSuffix(' | ngMeta - Dynamic meta tags for your Angular SPA');
 
-	}]);
+	}]).run(function(ngMeta) {
+    ngMeta.init();
+  });
